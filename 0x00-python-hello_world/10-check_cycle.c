46ce3db5fa2454e521e0e2fe0139592a309884e0
Sokayna <sokayna.bennasseur@gmail.com>
2023-06-06 01:32:09 +0000

    update

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has 
+ * cycle in it or not
+ * @list: a ppinter to a linked list
+ * Return: 0 if there is no cycle. 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *walking;
+	listint_t *driving;
+
+
+}
