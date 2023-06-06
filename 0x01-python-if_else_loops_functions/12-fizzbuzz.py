6ff1bdf886340f78482f42516f889c28f7d9f1af
Sokayna <sokayna.bennasseur@gmail.com>
2023-06-06 20:19:11 +0000

    Task12: Fix: end of the line

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,11 @@
 
 def fizzbuzz():
     for number in range(1, 101):
-        fizz = abs(number) % 3
-        buzz = abs(number) % 5
-        if fizz == 0:
+        if abs(number) % 3 == 0:
             print("Fizz", end=" ")
-        elif buzz == 0:
+        elif abs(number) % 5 == 0:
             print("Buzz", end=" ")
-        elif fizz == 0 and buzz == 0:
+        elif abs(number) % 3 == 0 and abs(number) % 5 == 0:
             print("FizzBuzz", end=" ")
         else:
             print("{}".format(number), end=" ")
