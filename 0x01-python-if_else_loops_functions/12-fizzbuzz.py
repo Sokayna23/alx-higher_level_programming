b01d6c5dad7dbda1cb185b00aade98a782e7ad38
Sokayna <sokayna.bennasseur@gmail.com>
2023-06-06 20:26:23 +0000

    Task12: Fix: FizzBuzz case

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 
 def fizzbuzz():
     for number in range(1, 101):
-        if abs(number) % 3 == 0:
+        if abs(number) % 3 == 0 and abs(number) % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif abs(number) % 3 == 0:
             print("Fizz", end=" ")
         elif abs(number) % 5 == 0:
             print("Buzz", end=" ")
-        elif abs(number) % 3 == 0 and abs(number) % 5 == 0:
-            print("FizzBuzz", end=" ")
         else:
             print("{}".format(number), end=" ")
