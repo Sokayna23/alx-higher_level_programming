560f8379b62b25908841d6b91f4f879fcae18b97
Sokayna <sokayna.bennasseur@gmail.com>
2023-06-07 01:18:17 +0000

    Task14: Print alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-for c in range(ord('Z'), ord('A') -1, -1):
-    char = chr(c)
-    print(char, end='')
-    if c % 2 == 0:
-        print(char.upper(), end="")
-print()
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
