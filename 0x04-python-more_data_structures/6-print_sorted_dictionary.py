6097289cb3b59abe412bf2dc1dd2ff79a4af7bb2
Sokayna <sokayna.bennasseur@gmail.com>
2023-06-14 18:11:55 +0000

    Task6: fix: string formating

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary.items())
     for (key, value) in sorted_dict:
-        print(key, ":", value)
+        print("{} : {}".format(key, value))
