cc16c560b1d71764ba85e206888cfd7e6420319d
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-03 11:52:35 -0400

    Task1: Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Square is a class that defines a square
+"""
+
+
+class Square:
+    """
+    Square class that defines a square by its size
+    """
+    def __init__(self, size):
+        self.__size = size
