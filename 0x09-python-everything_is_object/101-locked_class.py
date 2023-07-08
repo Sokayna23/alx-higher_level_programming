630009a5a0d22546e6bd07a4a8432a93b5ca2a41
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-08 13:23:29 -0400

    Task101: fix: pycodestyle

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,7 +4,7 @@
 
 class LockedClass:
     """ This class is to prevent the user from creating new instance
-        attributes, except if the new instance attribute is 
+        attributes, except if the new instance attribute is
         called first_name
     """
     __slots__ = ["first_name"]
