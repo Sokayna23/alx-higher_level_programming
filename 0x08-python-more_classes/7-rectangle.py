3f495d9d8ad9920898c04a48f50b712a77153959
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-09 16:18:44 -0400

    Task7: edit: initialization of print_symbol with #

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
             number_of_instances (int): the number of instances of Rectangle.
    """
     number_of_instances = 0
-    print_symbol = ''
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """Initialization function
