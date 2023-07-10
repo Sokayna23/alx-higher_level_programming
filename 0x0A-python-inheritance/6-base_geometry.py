5ba22dc430755c35ac8c66b36cbc6c821cb037df
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-10 12:07:23 -0400

    Task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Module that defines an empty class ''BaseGeomtry''
+"""
+
+
+class BaseGeometry:
+    """
+        An empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
