12928745de2d09f244488a782ca56c76f87bb461
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-10 09:12:54 -0400

    Task0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a lookup function that returns 
+    the list of available attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """Returns a list object of attributes and methodes.
+
+        Args:
+            obj (list): a list object.
+    """
+    return (dir(obj))
