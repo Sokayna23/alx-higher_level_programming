9c720353141c3fba5ffd806b036ddc73d9465369
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-11 17:38:08 -0400

    Task9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that defines Student class."""
+
+
+class Student:
+    """Defines a class Student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Istantiation of Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Gets a dictionary representation of the class Student."""
+        return self.__dict__
