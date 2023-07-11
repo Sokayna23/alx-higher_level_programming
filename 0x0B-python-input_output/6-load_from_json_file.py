089c4c107851bfa133763f921f5815639ef2a144
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-11 17:06:42 -0400

    Task6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines load_from_json_file() function."""
+import json
+
+
+def load_from_json_file(filename):
+    """
+        Creates an Object from a JSON file.
+
+        Args:
+            filename: file name.
+
+        Returns:
+            An Object.
+    """
+    with open(filename) as f:
+        return (json.load(f))
