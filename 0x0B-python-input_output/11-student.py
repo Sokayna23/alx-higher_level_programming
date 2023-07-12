4f7ce8ce82314b9dd96bcd4d5dfbdf6c0e04891a
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-11 21:27:07 -0400

    Task11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module that defines Student class."""
+
+
+class Student:
+    """Defines a class Student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Istantiation of Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gets a dictionary representation of the class Student."""
+        if (type(attrs) == list and
+                all(type(elmt) == str for elmt in attrs)):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for attr_name, attr_value in json.items():
+            setattr(self, attr_name, attr_value)
