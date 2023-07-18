02f30e48a195998ad67b50492cb4c61799b61fb0
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-17 21:24:58 -0400

    updeted

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -49,11 +49,11 @@ class Square(Rectangle):
                 self.__init__(self.width, self.height, self.x, self.y)
             else:
                 self.id = args[0]
-            if len(args) > 1:
+            if len(args) >= 1:
                 self.width = args[1]
-            if len(args) > 2:
+            if len(args) >= 2:
                 self.x = args[2]
-            if len(args) > 3:
+            if len(args) >= 3:
                 self.y = args[3]
         elif kwargs and len(kwargs) != 0:
             if "id" in kwargs:
