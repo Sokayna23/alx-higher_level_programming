0ee63c8d748dacad9d70ce691a000048c1e4e90f
Sokayna <sokayna.bennasseur@gmail.com>
2023-08-16 22:46:56 -0400

    Task13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_show_genres
 LEFT JOIN tv_genres
