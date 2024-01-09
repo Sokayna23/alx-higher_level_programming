38a0391d9567d659cc3075c62ed5e97093914d6d
Sokayna <sokayna.bennasseur@gmail.com>
2024-01-09 12:34:01 -0500

    Tas11: fix: logic to find the two largest num

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,17 +2,16 @@
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
-    for (let i = 2; i < process.argv.length; i++) {
-      if (process.argv[i] > process.argv[i + 1]) {
-        const max = process.argv[i];
-	const maxindex = i;
-      }
+  let max = Number.MIN_SAFE_INTEGER;
+  let max2 = Number.MIN_SAFE_INTEGER;
+  for (let i = 2; i < process.argv.length; i++) {
+    const currentNum = parseInt(process.argv[i]);
+    if (currentNum > max) {
+      max2 = max;
+      max = currentNum;
+    } else if (currentNum > max2 && currentNum < max) {
+      max2 = currentNum;
     }
-    for (let i = 2; i < process.argv.length; i++) {
-      if (process.argv[i] > process.argv[i + 1] && i !== maxindex) {
-        const max2 = process.argv[i]; 
-      }
-    }
-    console.log(max2);
   }
-
+  console.log(max2 === Number.MIN_SAFE_INTEGER ? 0 : max2);
+}
