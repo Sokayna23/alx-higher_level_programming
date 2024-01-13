4b2838ae0075fe01957138f879d314fb0a3ec965
Sokayna <sokayna.bennasseur@gmail.com>
2024-01-13 10:35:37 -0500

    task0: fix: pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,8 +4,11 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__" and len(sys.argv) == 4:
-    conn = MySQLdb.connect(host="localhost", port=3306, 
-            user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           password=sys.argv[2],
+                           db=sys.argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states")
     for row in cur.fetchall():
