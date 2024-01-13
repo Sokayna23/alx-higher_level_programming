58ec6d0ceab8852797bb1d3ae9d31d3cd517aa0e
Sokayna <sokayna.bennasseur@gmail.com>
2024-01-13 10:18:46 -0500

    Task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, 
             user="root", password="root", db="hbtn_0e_0_usa")
     cur = conn.cursor()
-    cur.execute("SELECT DISTINCT * FROM states")
+    cur.execute("SELECT * FROM states")
     for row in cur.fetchall():
         print(row)
     cur.close()
