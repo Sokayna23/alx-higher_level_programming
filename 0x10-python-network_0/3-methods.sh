df258683f592d999a5b8708416ca1b0dc052e232
Sokayna <sokayna.bennasseur@gmail.com>
2024-01-26 11:04:14 -0800

    task3: fix: script

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays all HTTP methods the server accept.
-curl -sI -X OPTIONS "$1" | awk '/Allow:/ { print $2 }'
+# Bash script that takes in a URL and displays all HTTP methods the server accepts.
+curl -sI -X OPTIONS "$1" | awk -F ': ' '/Allow:/ { print $2 }'
