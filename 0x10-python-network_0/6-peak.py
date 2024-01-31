aa7bfe8a8e66381aa49a3739e5929074ae7d2501
Sokayna <sokayna.bennasseur@gmail.com>
2024-01-31 16:34:31 -0500

    task6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """find_peak function module"""
 
-def find_peak(list_of_integers):
-	if not list_of_integers:
-		return (None)
-	n = len(list_of_integers);
-	left, right = 0, n - 1
-	while left < right:
-		mid = (left + right) // 2
-		if (list_of_integers[mid] > list_of_integers[mid + 1]):
-			right = mid
-		else:
-			left = mid + 1
-	return list_of_integers[right] if right > 0 else list_of_integers[0]
 
+def find_peak(list_of_integers):
+    """Determines the peak integer in a list"""
+    peak = None
+    for i in list_of_integers:
+        if peak is None or i > peak:
+            peak = i
+        else:
+            break
+    return peak
