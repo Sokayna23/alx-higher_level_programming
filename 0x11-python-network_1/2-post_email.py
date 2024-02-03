fe93ed4a346a21bac0eed66fd3039ac80ea595ad
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-03 15:53:50 -0500

    task2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module that POST an email """
+from urllib import request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = parse.urlencode({'email': email}).encode('utf-8')
+    req = request.Request(url, data=data, method='POST')
+    with request.urlopen(url) as response:
+        print(response.read().decode('utf-8'))
