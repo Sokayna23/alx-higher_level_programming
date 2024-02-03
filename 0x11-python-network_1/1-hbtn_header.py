706a0b149ef0ed50c7a687020b0dcbb5e31ed45d
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-03 15:23:09 -0500

    ta1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,10 @@
 import sys
 from urllib import request
 
+
 url = sys.argv[1]
-if __name__ == "__main__" and len(sys.argv) == 2 :
-	with request.urlopen(url) as response:
-            headers = response.info()
-            id = headers.get('X-Request-Id')
-            print(id)
+if __name__ == "__main__" and len(sys.argv) == 2:
+    with request.urlopen(url) as response:
+        headers = response.info()
+        id = headers.get('X-Request-Id')
+        print(id)
