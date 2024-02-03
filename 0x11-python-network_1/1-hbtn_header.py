1d8750357f6e732cbabed0fd0e37a91b95aaefd3
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-03 15:37:36 -0500

    module passss

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,8 @@ from urllib import request
 import sys
 
 
-url = sys.argv[1]
 if __name__ == "__main__":
+    url = sys.argv[1]
     with request.urlopen(url) as response:
         headers = response.info()["X-Request-Id"]
         print(headers)
