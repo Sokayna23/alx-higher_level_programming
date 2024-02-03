687027bb4d3087aa891e275aa0c1ab0f1703a9cf
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-03 14:50:10 -0500

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 """script that fetches https://alx-intranet.hbtn.io/status"""
 from urllib import request
 
-url = "https://alx-intranet.hbtn.io/status"
-if __name__ == "__main__":
-	with request.urlopen(url) as response:
-		content = response.read().decode('utf-8')
 
-		print("Body response:")
-		print("\t- type:", type(content))
-		print("\t- content:", content)
-		print("\t- utf8 content:", content)
+url = "https://alx-intranet.hbtn.io/status"
+
+with request.urlopen(url) as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", html.decode('utf-8'))
