5eb8abcecf22c4fc1a39c474fad9b612958622ca
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-04 12:03:22 -0800

    error print

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
         url = sys.argv[1]
         response = requests.get(url)
         response.raise_for_status()
+        print(response.text)
     except requests.exceptions.HTTPError as err:
         if err.response.status_code >= 400:
             print("Error code:", err.response.status_code)
-        elif err.response.status_code == 200:
-            print(response.text)
