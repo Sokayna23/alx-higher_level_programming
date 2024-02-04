31ee9f170dec1a745763350657f44f2e8dc8e8b8
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-04 10:22:50 -0800

    fix: decode the body

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     req = request.Request(url)
     try:
         with request.urlopen(req) as response:
-            pass
+            print(response.read().decode('utf-8'))
     except error.HTTPError as e:
         print("Error code:", e.code)
+
