31797d1ff56b6bbd9f29eefd450d8b381ddf83de
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-04 13:26:52 -0800

    task9' git push

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module: My GitHub! """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/users/" + username
+    data = {"Authorization": password}
+    response = requests.get(url, headers=data)
+    json = response.json()
+    print(json.get("id"))
