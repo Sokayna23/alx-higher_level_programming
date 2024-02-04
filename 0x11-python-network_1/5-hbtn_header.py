1fef6c107fcf73c39ccc4726c507db977e329787
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-04 10:58:29 -0800

    task5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module:  Response header value """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    x_request_id = req.headers['X-Request-Id']
+    print(x_request_id)
