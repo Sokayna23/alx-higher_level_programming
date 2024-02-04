4272439d09cf79810521d8e8ea95201fd69dc6d4
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-04 12:47:24 -0800

    task8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module: Search API """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) == 2 else ""
+    try:
+        response = requests.post(url, data={'q': q})
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
