6cf25e248f73cc8d1f1c3cd026b07e35922db239
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-05 04:49:15 -0800

    fix pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     REPO = sys.argv[1]
     OWNER = sys.argv[2]
     count = 10
-    url = f"https://api.github.com/repos/{OWNER}/{REPO}/commits?per_page={count}"
+    url = f"https://api.github.com/repos/{OWNER}/{REPO}\
+            /commits?per_page={count}"
     params = {"author": OWNER, "per_page": count}
 
     response = requests.get(url, params=params)
     commits = response.json()
-    
     for commit in commits:
         name = commit.get("commit").get("author").get("name")
         print(f'{commit.get("sha")}: {name}')
