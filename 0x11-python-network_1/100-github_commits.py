9f1256557026ef7a0104a0371b67cc24c3e52366
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-05 04:54:45 -0800

    fix url

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,11 +8,10 @@ if __name__ == "__main__":
     REPO = sys.argv[1]
     OWNER = sys.argv[2]
     count = 10
-    url = f"https://api.github.com/repos/{OWNER}/{REPO}\
-            /commits?per_page={count}"
-    params = {"author": OWNER, "per_page": count}
+    url = f"https://api.github.com/repos\
+/{OWNER}/{REPO}/commits?per_page={count}"
 
-    response = requests.get(url, params=params)
+    response = requests.get(url)
     commits = response.json()
     for commit in commits:
         name = commit.get("commit").get("author").get("name")
