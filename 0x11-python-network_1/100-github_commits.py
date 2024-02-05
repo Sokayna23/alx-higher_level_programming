94430761453665531f8222ea81a02225145dec93
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-05 04:45:51 -0800

    task100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Interview: list commits """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    REPO = sys.argv[1]
+    OWNER = sys.argv[2]
+    count = 10
+    url = f"https://api.github.com/repos/{OWNER}/{REPO}/commits?per_page={count}"
+    params = {"author": OWNER, "per_page": count}
+
+    response = requests.get(url, params=params)
+    commits = response.json()
+    
+    for commit in commits:
+        name = commit.get("commit").get("author").get("name")
+        print(f'{commit.get("sha")}: {name}')
