ea75bb4c53fde46f4f3f2efaf9ce39ed81d0ea29
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-05 03:42:26 -0800

    Task10' git push

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" module: My GitHub! """
+""" module: My GitHub ID """
 import requests
 import sys
 
@@ -7,11 +7,10 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
+
     url = "https://api.github.com/users/" + username
-    data = {"Authorization": password}
-    response = requests.get(url, headers=data)
-    json = response.json()
-    if (response.status_code >= 400):
-        print("None")
-        exit()
-    print(json.get("id"))
+
+    data = {"Authorization": "token {}".format(password)}
+
+    resp = requests.get(url, headers=data)
+    print(resp.json().get("id"))
