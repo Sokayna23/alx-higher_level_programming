63b13066ad36c146b3d113e81829ad10a636835a
sokayna <sokayna.bennasseur@gmail.com>
2024-02-20 16:52:01 -0500

    t5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,6 +3,7 @@
 const request = require('request');
 const process = require('process');
 const fs = require('fs');
+
 const url = process.argv[2];
 const filePath = process.argv[3];
 
@@ -15,9 +16,8 @@ request.get(url, (error, resp, body) => {
     fs.writeFile(filePath, body, 'utf-8', (err) => {
       if (err) {
         console.error(err);
-      } else {
-	console.log(filePath);
       }
     });
   }
 });
+
